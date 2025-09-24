d536dbd3e657112d58e9d30394d45e686efe7d82
WT <go.to.another5@gmail.com>
2025-09-24 19:28:21 +0300

    Update to 0.1.1

---
+++ HEVLIB_EQUIPMENT_DRIVER_TAGS/WEAPONSLOT_SHIP_MODIFY.gd
@@ -0,0 +1,40 @@
+extends Node
+
+const WEAPONSLOT_SHIP_MODIFY = {
+	"SHIP_TORPEDO_BOAT":{
+		"sailCenter":{
+			"SYSTEM_MWTIGHTBEAM":[
+				{
+					"property":"position",
+					"value":"Vector2( 0, -40 )"
+				}
+			],
+			"SYSTEM_MWTIGHTBEAM_PDT":[
+				{
+					"property":"position",
+					"value":"Vector2( 0, -20 )"
+				}
+			],
+			"SYSTEM_DND_HARVHAUL":[
+				{
+					"property":"position",
+					"value":"Vector2( 0, 0 )"
+				},
+				{
+					"property":"z_index",
+					"value":"-16"
+				}
+			],
+			"SYSTEM_DND_HARVTUG":[
+				{
+					"property":"position",
+					"value":"Vector2( 0, 0 )"
+				},
+				{
+					"property":"z_index",
+					"value":"-16"
+				}
+			]
+		}
+	}
+}
